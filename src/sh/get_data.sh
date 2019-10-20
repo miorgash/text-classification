c107067ddb9ea0db7e360d21045be3c417df1a8f
Miori Igarashi <miori@MiorinoMacBook-Pro.local>
2019-10-20 19:14:42 +0900

    init

---
+++ src/sh/get_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl https://www.rondhuit.com/download/ldcc-20140209.tar.gz -o ${HOME}/text-classification/data/ro/ldcc-20130209.tar.gz
+tar xvzf ${HOME}/text-classification/data/ro/ldcc-20130209.tar.gz -C ${HOME}/text-classification/data/ro/
